90a5b99f1af4e1353b7f871fc7694c844395706d
minseok <imuchgabis@gmail.com>
2024-04-10 19:04:51 +0900

    풀이완료

--- 성민석/해시/전화번호목록.js
+++ 성민석/해시/전화번호목록.js
@@ -1,5 +1,6 @@
 function solution(phone_book) {
   phone_book.sort();
+  console.log(phone_book);
   for (let i = 0; i < phone_book.length - 1; i++) {
     let a = phone_book[i + 1].slice(0, phone_book[i].length);
     if (phone_book[i] === a) {
